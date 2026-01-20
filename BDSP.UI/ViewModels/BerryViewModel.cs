7bb7c43c295a5a81fb53723da928a1c35b88497a
GlassToeStudio <GlassToeStudio@gmail.com>
2026-01-20 08:12:49 -0600

    Sart adding color

--- BDSP.UI/ViewModels/BerryViewModel.cs
+++ BDSP.UI/ViewModels/BerryViewModel.cs
@@ -4,22 +4,39 @@ using System.Linq;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using BDSP.Core.Berries;
-
+using Avalonia.Media;
+using BDSP.UI.Styling;
 namespace BDSP.UI.ViewModels
 {
 
     public sealed class BerryViewModel(Berry berry)
     {
+        IBrush PrimaryFlavorBrush => PrimaryFlavor switch
+        {
+            "Spicy" => FlavorColors.Spicy,
+            "Dry" => FlavorColors.Dry,
+            "Sweet" => FlavorColors.Sweet,
+            "Bitter" => FlavorColors.Bitter,
+            _ => FlavorColors.Sour
+        };
+
+        IBrush RarityBrush => Rarity switch
+        {
+            "Common" => FlavorColors.Common,
+            "Uncommon" => FlavorColors.Uncommon,
+            _ => FlavorColors.Rare
+        };
+
         public Berry Berry { get; } = berry;
         public string Name => $"{Berry.Id.GetName()}";
         public string PrimaryFlavor => GetPrimaryFlavor(Berry);
         public int Smoothness => Berry.Smoothness;
         public string Rarity => GetRarity(Berry);
-        public int Spicy => Berry.Spicy;    
+        public int Spicy => Berry.Spicy;
         public int Dry => Berry.Dry;
-        public int Sweet => Berry.Sweet;    
-        public int Bitter => Berry.Bitter;  
-        public int Sour => Berry.Sour;  
+        public int Sweet => Berry.Sweet;
+        public int Bitter => Berry.Bitter;
+        public int Sour => Berry.Sour;
         public Bitmap Image
         {
             get
@@ -52,5 +69,7 @@ namespace BDSP.UI.ViewModels
                    b.Smoothness <= 30 ? "Uncommon" :
                    "Rare";
         }
+
+
     }
 }
